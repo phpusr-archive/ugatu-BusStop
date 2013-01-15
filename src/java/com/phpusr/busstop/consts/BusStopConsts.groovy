6fcc9a6bdfec4e52e2d0945ed10aef72bafcf25e
phpusr <phpusr@gmail.com>
2013-01-15 16:03:28 +0600

    Добавлен константы

---
+++ src/java/com/phpusr/busstop/consts/BusStopConsts.groovy
@@ -0,0 +1,19 @@
+package com.phpusr.busstop.consts
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phpusr
+ * Date: 15.01.13
+ * Time: 15:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Константы
+ */
+class BusStopConsts {
+    /** Вкл/Выкл логирование в Bus */
+    static final boolean busLog = false
+    /** Вкл/Выкл логирование в BusStopFrame.paint() */
+    static final boolean paintLog = true
+}
