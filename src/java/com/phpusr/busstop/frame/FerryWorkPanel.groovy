a78d7e3a3e9918bfbc597c49c29112f8bd46fd5e
phpusr <phpusr@gmail.com>
2013-01-25 14:33:59 +0600

    Изменена анимация движения пассажиров

--- src/java/com/phpusr/busstop/frame/FerryWorkPanel.groovy
+++ src/java/com/phpusr/busstop/frame/FerryWorkPanel.groovy
@@ -23,7 +23,7 @@ import java.awt.image.BufferedImage
 class FerryWorkPanel extends JPanel {
 
     /** Координаты рисования Пассажиров */
-    int xPosBus, yPosPassenger
+    int xPosBus, xPosPassenger
     /** Кол-во Пассажиров для Выхода и Входа */
     int passengerCountOut, passengerCountOutConst, passengerCountIn, passengerCountInConst
     /** Кол-во остановок */
@@ -90,13 +90,13 @@ class FerryWorkPanel extends JPanel {
         if (xPosBus <= 0 || xPosBus >= FerryWorkConsts.WIDTH-ferry.width) {
             if (!stop) { //Если зашли в это условие первый раз, то генерируем кол-во входящих пассажиров
                 revers = !revers
+                xPosPassenger = revers ? FerryWorkConsts.WIDTH-ferry.width/2 : ferry.width/2
                 stopCount++
                 passengerCountOut = Math.round(Math.random() * ferry.passengerCount)
                 passengerCountOutConst = passengerCountOut
                 passengerCountIn = Math.round(Math.random() * ferry.freeSeat)
                 passengerCountInConst = passengerCountIn
                 out = true
-                yPosPassenger = FerryWorkConsts.Y_POS + ferry.height/2
 
                 if (FerryWorkConsts.busLog) println '-----------------------------------------------------------------------------------'
                 if (FerryWorkConsts.paintLog) println "$stopCount\t Кол-во выходящих пассажиров: ${passengerCountOut}/$ferry.passengerCount,\t\t Кол-во входящих пассажиров: ${passengerCountIn}/$ferry.freeSeat"
@@ -108,26 +108,27 @@ class FerryWorkPanel extends JPanel {
             if (out) { //Анимация выхода пассажиров
                 stop = true
                 if (passengerCountOut > 0) {
-                    drawUtil.drawPassenger(scrnG, (int)FerryWorkConsts.WIDTH/2, yPosPassenger)
-                    yPosPassenger += FerryWorkConsts.PIXEL_INC
-                    if (yPosPassenger > FerryWorkConsts.HEIGHT) { //Пассажир вышел из Автобуса
+                    drawUtil.drawPassenger(scrnG, xPosPassenger, (int)FerryWorkConsts.HEIGHT/3)
+                    //Если True - значит пассажиры выходят Справа
+                    xPosPassenger = revers ? xPosPassenger + FerryWorkConsts.PIXEL_INC : xPosPassenger - FerryWorkConsts.PIXEL_INC
+                    if (revers && xPosPassenger > FerryWorkConsts.WIDTH || !revers && xPosPassenger < 0) { //Пассажир вышел из Парома
                         passengerCountOut--
                         ferry.delPassenger()
-                        yPosPassenger = FerryWorkConsts.Y_POS + ferry.height/2
+                        xPosPassenger = revers ? FerryWorkConsts.WIDTH-ferry.width/2 : ferry.width/2
                         updateBusInfo()
                     }
                 } else {
                     out = false
-                    yPosPassenger = FerryWorkConsts.HEIGHT
+                    xPosPassenger = revers ? FerryWorkConsts.WIDTH : 0
                 }
             } else { //Анимация входа пассажиров
                 if (passengerCountIn > 0) {
-                    drawUtil.drawPassenger(scrnG, (int)FerryWorkConsts.WIDTH/2, yPosPassenger)
-                    yPosPassenger -= FerryWorkConsts.PIXEL_INC
-                    if (yPosPassenger < FerryWorkConsts.Y_POS + ferry.height/2) { //Пассажир сел на Автобус
+                    drawUtil.drawPassenger(scrnG, xPosPassenger, (int)FerryWorkConsts.HEIGHT/3)
+                    xPosPassenger = revers ? xPosPassenger - FerryWorkConsts.PIXEL_INC : xPosPassenger + FerryWorkConsts.PIXEL_INC
+                    if (revers && xPosPassenger < FerryWorkConsts.WIDTH-ferry.width/2 || !revers && xPosPassenger > ferry.width/2) { //Пассажир сел на Паром
                         passengerCountIn--
                         ferry.addPassenger()
-                        yPosPassenger = FerryWorkConsts.HEIGHT
+                        xPosPassenger = revers ? FerryWorkConsts.WIDTH : 0
                         updateBusInfo()
                     }
                 } else {
