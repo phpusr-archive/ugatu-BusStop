188efdd72d2d101a78ac385b51b9e56942539d27
phpusr <phpusr@gmail.com>
2013-01-27 14:21:50 +0600

    Добавил форму ввода Параметров

---
+++ src/java/com/phpusr/ferrywork/frame/ParamsFrame.java
@@ -0,0 +1,18 @@
+package com.phpusr.ferrywork.frame;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phpusr
+ * Date: 27.01.13
+ * Time: 14:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class ParamsFrame {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton OKButton;
+    private JTextField textField3;
+}
