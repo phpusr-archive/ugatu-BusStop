e33ab046107843abd1de60e7ea2b4e7b57a7c802
phpusr <phpusr@gmail.com>
2013-01-27 21:46:03 +0600

    Добавил батник создания релиза

---
+++ release/build.bat
@@ -0,0 +1,5 @@
+rem copy ..\doc\kursach.docx "ООП ПЗ-Курсовая работа (Доронин С. ПРО-201в).docx"
+rem copy ..\out\artifacts\BusStop_jar\BusStop.jar BusStop.jar
+cd ..
+rem ant
+gradlew.bat build
--- release/copy.bat
+++
@@ -1,4 +0,0 @@
-copy ..\doc\kursach.docx "ООП ПЗ-Курсовая работа (Доронин С. ПРО-201в).docx"
-rem copy ..\out\artifacts\BusStop_jar\BusStop.jar BusStop.jar
-cd ..
-ant
